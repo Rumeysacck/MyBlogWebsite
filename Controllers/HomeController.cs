ed4e496373c66e5671884adf9f375a6dcdb5f431
rumeysacck <rumeysa7cicek@gmail.com>
2024-09-30 10:16:56 +0300

    model name changed

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,8 +1,8 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using MyBlghWebsite.Models;
+using MyBlogWebsite.Models;
 
-namespace MyBlghWebsite.Controllers;
+namespace MyBlogWebsite.Controllers;
 
 public class HomeController : Controller
 {
--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -1,4 +1,4 @@
-namespace MyBlghWebsite.Models;
+namespace MyBlogWebsite.Models;
 
 public class ErrorViewModel
 {
