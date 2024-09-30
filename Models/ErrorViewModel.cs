34d734bd87d07fc35bbab588f58f7517a564ef93
rumeysacck <rumeysa7cicek@gmail.com>
2024-09-30 09:59:51 +0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MyBlghWebsite.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
