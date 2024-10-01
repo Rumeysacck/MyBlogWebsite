fb806aed5b34250fc20fb589d1432fa9c7dc95c1
rumeysacck <rumeysa7cicek@gmail.com>
2024-10-01 23:28:44 +0300

    contact info section has been updated

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('#altIcerik').addClass('gorunur');
+        } else {
+            $('#altIcerik').removeClass('gorunur');
+        }
+    });
+});
